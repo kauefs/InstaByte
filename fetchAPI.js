bbaa15b1f19a76932d2b9168cc3f31d7013b12c6
KAUE <139394184+kauefs@users.noreply.github.com>
2024-11-25 14:09:24 -0300

    fetchAPI.js

--- fetchAPI.js
+++ fetchAPI.js
@@ -1,7 +1,20 @@
-let{API_URL}=process.env;
-// Fetch EndPoint Data Function:
-export default async function fetchImages() {
-  try {const response = await fetch(process.env.API_URL); // Use imported URL
-       const   data   = await      response.json(      );
-       return  data}
-       catch (error) {console.error('Error Loading Data:', error)}}
+// let{API}=process.env;
+// // Fetch EndPoint Data Function:
+// export default async function fetchImages() {
+//   try {const response = await fetch(process.env.API); // Use imported URL
+//        const   data   = await      response.json(      );
+//        return  data}
+//        catch (error) {console.error('Error Loading Data:', error)}}
+
+import type {Config, Context} from '@netlify/edge-functions'
+export default async (request : Request, context : Context) => {
+  const response = await context.next()
+  return new Response(response.body, {
+    headers: {
+       'access-control-allow-origin': '*'
+    }
+  })
+}
+export const config : Config = {
+  path: '/*'
+}
